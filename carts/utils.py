e8155e8ac3ee584f2f33c234f03a0041da156de9
magwindow <antosha.petrakov@yandex.ru>
2024-10-20 18:48:00 +0300

    Sessions - carts for anonymous user

--- carts/utils.py
+++ carts/utils.py
@@ -4,3 +4,9 @@ from carts.models import Cart
 def get_user_carts(request):
     if request.user.is_authenticated:
         return Cart.objects.filter(user=request.user)
+    
+    
+    if not request.session.session_key:  # если нет сессионного ключа
+        request.session.create()  # создаем сессионный ключ
+        
+    return Cart.objects.filter(session_key=request.session.session_key)
