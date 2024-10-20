5ec6a3fe8af03bb724cf93cbc383b9d450470768
magwindow <antosha.petrakov@yandex.ru>
2024-10-20 20:01:50 +0300

    Admin panel setup

--- goods/admin.py
+++ goods/admin.py
@@ -8,9 +8,24 @@ from goods.models import Categories, Products
 
 @admin.register(Categories)
 class CategoriesAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('name', )}
-    
-    
+    prepopulated_fields = {"slug": ("name",)}
+
+
 @admin.register(Products)
 class ProductsAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('name', )}
+    prepopulated_fields = {"slug": ("name",)}
+
+    # редактирование полей в админке
+    list_display = ["name", "quantity", "price", "discount"]
+    list_editable = ["discount"]
+    search_fields = ["name", "description"]
+    list_filter = ["discount", "quantity", "category"]
+    fields = [
+        "name",
+        "category",
+        "slug",
+        "description",
+        "image",
+        ("price", "discount"),
+        "quantity",
+    ]
