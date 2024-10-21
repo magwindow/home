c2751964f6e88320e8253cbfc0d2b16f4f682885
magwindow <antosha.petrakov@yandex.ru>
2024-10-21 17:41:41 +0300

    Class based views

--- main/views.py
+++ main/views.py
@@ -1,25 +1,26 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
 from goods.models import Categories
 
 
-def index(request):
-    
-    
-    context = {
-        'title': 'HOME - Главная',
-        'content': 'Магазин мебели HOME',
-    }
-    
-    return render(request, 'main/index.html', context)
 
-
-def about(request):
-    context = {
-        'title': 'HOME - О нас',
-        'content': 'О нас',
-        'text_on_page': 'Текст о том почему этот магазин такой классный и какой хороший товар'
-    }
+class IndexView(TemplateView):
+    template_name = 'main/index.html'
     
-    return render(request, 'main/about.html', context)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'HOME - Главная'
+        context['content'] = 'Магазин мебели HOME'
+        return context
+    
+    
+class AboutView(TemplateView):
+    template_name = 'main/about.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'HOME - О нас'
+        context['content'] = 'О нас'
+        context['text_on_page'] = 'Текст о том почему этот магазин такой классный и какой хороший товар'
+        return context
+
